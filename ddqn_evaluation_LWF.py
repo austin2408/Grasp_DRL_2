2a433c7b8f3b571ca18f3c3f292c5f8d1543e41a
austin2408 <sdnd93612@gmial.com>
2021-11-23 13:11:21 +0800

    fix

--- ddqn_evaluation_LWF.py
+++ ddqn_evaluation_LWF.py
@@ -4,6 +4,7 @@ import scipy.misc
 import numpy as np
 import torch
 import os
+import json
 from math import *
 import h5py
 import matplotlib.pyplot as plt
@@ -15,10 +16,10 @@ warnings.filterwarnings("ignore")
 
 net = reinforcement_net_new(use_cuda=True)
 
-model_name = 'weight/behavior_1000_178715_LWF.pth'
+model_name = 'weight/behavior_LWF_10_1500_0.00.pth'
+net.load_state_dict(torch.load(model_name))
 # net.grasp_net.load_state_dict(torch.load('weight/grasp_net.pth'))
 # net.value_net.load_state_dict(torch.load('weight/value_net.pth'))
-net.load_state_dict(torch.load(model_name))
 net = net.cuda().eval()
 
 # torch.save(net.grasp_net.state_dict(), os.path.join(
@@ -27,36 +28,35 @@ net = net.cuda().eval()
 # torch.save(net.value_net.state_dict(), os.path.join(
 #             '/home/austin/Grasp_DRL_2/weight', "value_net.pth"))
 
-# hdf5_path = 'Logger05.hdf5'
-# f = h5py.File(hdf5_path, "r")
-# angle = [90, -45, 0, 45]
-# dis_count = 0
-# theta_count = 0
-# angle_label = []
-# angle_pred = []
-# count = 0
 
-L = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets')
-for name in L[:1]:
-    print(name)
-    A = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/rgb')
+L = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets_view')
+# L.sort()
+# print(L)
+up = [0,0]
+fr = [0,0]
+x = []
+y = []
+for name in L:
+# for name in L[:1]:
+# for name in L[345:355]:
+    # print(name)
+    print(100*(up[1]+fr[1])/len(L), end='\r')
+    num = int(name.split('_')[1])
+    A = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/rgb')
     A.sort()
-    B = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/depth')
+    B = os.listdir('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/depth')
     B.sort()
     # print(B)
     # print(A)
 
     
 
-    color = cv2.imread('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/rgb/'+A[0])
+    color = cv2.imread('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/rgb/'+A[0])
     # print(color.shape)
     color = cv2.resize(color, (224,224))
-    depth = np.load('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/depth/'+B[0])
+    depth = np.load('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/depth/'+B[0])
     depth[depth>10000] == 0
-    print(depth.shape)
 
-    # print(color.shape)
-    # print(depth.shape)
 
     color_tensor, depth_tensor, pad = preprocessing(color, depth)
     color_tensor = color_tensor.cuda()
@@ -65,74 +65,69 @@ for name in L[:1]:
     with torch.no_grad():
             prediction, prediction_new = net.forward(color_tensor, depth_tensor, is_volatile=True)
 
-    _,_,aff, out = postProcessing(prediction, color, depth, color_tensor, pad, show=False)
-    for i in prediction_new[0][0][0][:1]:
-        print(i)
-    print(len(prediction_new))
-    # print(prediction_new[0].shape)
-    # print(len(prediction))
-    # print(prediction[0].shape)
-    print(torch.max(prediction_new[0]))
+    _,_,aff, out, view = postProcessing(prediction, prediction_new, color, depth, color_tensor, pad, show=False)
+    # print(view.shape)
+    id = np.where(view == np.max(view))
+    cv2.circle(view, (id[1][0], id[0][0]), 3, (0, 0, 0), 2)
+    # print(id[1][0])
+    x.append(id[0][0])
+    y.append(id[1][0])
+    with open('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/rgb/'+A[1],"r") as F:
+        data = json.load(F)
+        if data['shapes'][0]['label'] == 'upper':
+            if num < 350:
+                up[1] += 1
+                if id[1][0] < 112:
+                    up[0] += 1
+            else:
+                fr[1] += 1
+                if id[1][0] > 112:
+                    fr[0] += 1
 
-    plt.imshow(aff[:,:,[2,1,0]])
-    # os.mkdir('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/Qmap')
-    # np.save('/home/austin/Test_ws/src/collect/src/Datasets/'+name+'/Qmap/qmap', out[0][0])
+        else:
+            if num < 350:
+                fr[1] += 1
+                if id[1][0] > 112:
+                    fr[0] += 1
+            else:
+                up[1] += 1
+                if id[1][0] < 112:
+                    up[0] += 1
+
+    # fig = plt.figure(figsize=(10, 10))
+    # fig.add_subplot(1, 3, 1)
+    # plt.imshow(color[:,:,[2,1,0]])
+
+    # fig.add_subplot(1, 3, 2)
+    # plt.imshow(aff[:,:,[2,1,0]])
+    # # plt.imshow(out[0][0])
+
+    # fig.add_subplot(1, 3, 3)
+    # plt.imshow(view)
+
+    # plt.show()
+
+    # os.mkdir('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/Qmap')
+    # np.save('/home/austin/Test_ws/src/collect/src/Datasets_view/'+name+'/Qmap/qmap', out[0][0])
     # norm = np.linalg.norm(out[0][0])
     # out[0][0] = out[0][0]/norm
     # print(out[0][0])
     # plt.imshow(out[0][0])
-    plt.show()
 
+    # plt.show()
+print(up[0]/up[1])
+# print(up[0],up[1])
+print(fr[0]/fr[1])
+plt.scatter(y, x)
+# plt.savefig('V6_1000.png', dpi=300)
+plt.show()
 
-# # file = open(path+'/Test_record2.txt', "a+")
-# for name in f.keys():
-#     group = f[name]
-#     action = group['action'][()]
-#     color = f[name+'/state/color'][()]
-#     depth = f[name+'/state/depth'][()]
-#     reward = group['reward'][()]
-#     if reward > 0:
-#         count += 1
-#         color_tensor, depth_tensor, pad = preprocessing(color, depth)
-#         color_tensor = color_tensor.cuda()
-#         depth_tensor = depth_tensor.cuda()
-        
-#         with torch.no_grad():
-#             prediction = net.forward(color_tensor, depth_tensor, is_volatile=True)
-
-        
-#         result,_,_ = postProcessing(prediction, color, depth, color_tensor, pad, show=False)
-#         theta = angle[int(action[0])]
-#         angle_pred.append(int(result[0]))
-#         angle_label.append(int(theta))
-#         print(name)
-#         print('Pred : ',result)
-#         print('Label : ',[theta, action[1], action[2]])
-#         plt.show()
-#         dis_error = abs(sqrt((result[1] - action[1])**2 + (result[2] - action[2])**2))
-#         if (int(result[0]) !=  int(theta)):
-#             theta_count += 1
-#         print('Dis_error : ',dis_error)
-#         if dis_error > 20:
-#             dis_count += 1
-
-#         print('===================================================')
-#     # file.write(name+'\n'+'Pred : '+str(result)+'\n'+'Label : '+str([theta, action.value[1], action.value[2]])+'\n'+'Dis_error : '+str(dis_error)+'\n')
-#     # file.write('==================================================='+'\n')
-
-# print('Accuracy of position : ',(1 - dis_count/count))
-# print('Accuracy of theta : ',(1 - theta_count/count))
-# # file.write(str((1 - dis_count/len(f.keys())))+'/'+str((1 - theta_count/len(f.keys()))))
-# # file.close()
-
-# matrix = np.zeros((4,4))
-# for i in range(len(angle_pred)):
-#     pred = angle.index(angle_pred[i])
-#     label = angle.index(angle_label[i])
-#     matrix[pred, label] += 1
-
-# print('     90 -45 0 45')
-# print(' 90', matrix[0])
-# print('-45', matrix[1])
-# print('  0', matrix[2])
-# print(' 45', matrix[3])
+#IfiveOO
+# vtwo0.17624521072796934
+# 0.7782426778242678
+# vthree0.29118773946360155
+# 0.7280334728033473
+# vfour0.20306513409961685
+# 0.8326359832635983
+# vfive0.5977011494252874
+# 0.4225941422594142
